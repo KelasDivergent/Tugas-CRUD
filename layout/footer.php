0749e0244a19e6117425443731a7662b26193730
Your hasbyaiman06 <hasbyaimanzzz123@gmail.com>
2023-09-09 23:28:11 +0700

    first commit

---
+++ layout/footer.php
@@ -0,0 +1,18 @@
+    <!-- Footer Start -->
+    <footer class="bg-info" id="footer">
+        <div class="container">
+        </div>
+        <!-- Copyright -->
+        <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+            © 2023 Copyright Made With ♥
+            <a class="text-reset" href="/">Hasby Aiman</a>
+        </div>
+        <!-- Copyright -->
+    </footer>
+    <!-- Footer End -->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</body>
+
+</html>
